f8e1208d28dd3a5e7880d42c6fa650e8e0a46593
弥和 <a.cokegasuki@gmail.com>
2023-09-28 20:24:48 +0900

    データテーブルからの絶対参照

--- test.py
+++ test.py
@@ -1,16 +1,22 @@
 import pandas as pd
 
 ranking_df = pd.read_csv('ranking.csv')
-print(ranking_df)
+
 
 user_anser = input('>>')
 user_anser = user_anser.split()
 new_user_anser = ''
+No_user_anser = ''
 for ua in user_anser:
     new_user_anser = new_user_anser + ' ' + ua.capitalize()
+    new_user_anser = str.strip(new_user_anser)
     if new_user_anser == 'N':
-        new_user_anser = 'No'
+        No_user_anser = 'No'
 
-if new_user_anser == 'No':
+print(ranking_df[ranking_df['Name'].isin([new_user_anser])])
 
-#https://qiita.com/ryu022304/items/e5a3b470795de883a09a
+
+
+
+
+
