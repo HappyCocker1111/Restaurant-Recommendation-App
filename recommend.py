05a6fe17aa69f75c4dede7246faa4e1938804f17
弥和 <a.cokegasuki@gmail.com>
2023-10-01 21:35:17 +0900

    if条件分岐countでの修正

--- recommend.py
+++ recommend.py
@@ -23,8 +23,8 @@ for ua in user_anser:
     if new_user_anser == 'N':
         No_user_anser = 'No'
 
-returen_data = ranking_df[ranking_df['Count'].isin([new_user_anser])]
-if len(returen_data.index) == 0:
+
+if len(ranking_df['Count']) == 0:
     Result = pd.DataFrame({'Name': [new_user_anser], 'Count': [1]})
     Result.to_csv("ranking.csv",index=False, encoding="utf-8",header=True)
     print(user_name +'さん。ありがとうございました。\n良い一日を！さようなら。')
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 ranking_df = pd.read_csv('ranking.csv')
-print(ranking_df['Name'])
+print(ranking_df)
 
 user_anser = input('>>')
 user_anser = user_anser.split()
@@ -13,13 +13,14 @@ for ua in user_anser:
     if new_user_anser == 'N':
         No_user_anser = 'No'
 
-returen_data = ranking_df[ranking_df['Count'].isin([new_user_anser])]
 
-if len(returen_data.index) == 0:
+
+if len(ranking_df['Count']) == 0:
     new_row = {'Name': new_user_anser, 'Count': 1}
     ranking_df = pd.concat([ranking_df, pd.DataFrame([new_row])], ignore_index=True)
     print('xyz')
-
+else:
+    print('abc')
 
 
 
